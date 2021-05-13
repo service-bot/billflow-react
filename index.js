42a18ab51fc406a567e917ba139610aa860452ce
Ka Lung Sy <kalungsy@gmail.com>
2021-05-13 10:36:02 -0400

    remove export of the embed component

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import useBillflowEmbed from './hooks/useBillflowEmbed'
-import BillflowEmbed from './components/BillflowEmbed'
 
 export {
-    useBillflowEmbed,
-    BillflowEmbed
+    useBillflowEmbed
 }
